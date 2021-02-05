83d52e751b100bd00486231788a25089bcae7f7c
Alex <aviktorov457@gmail.com>
2021-02-05 19:47:00 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import zipfile
+from docxtpl import DocxTemplate
+from docx2pdf import convert
+import codecs
+Date1 = 12.01
+Date2 = 13.01
+Olympiad = "Городская олимпиада по математике"
+
+z = zipfile.ZipFile('zipfile.zip', 'w')
+f = codecs.open('Data.txt', 'r', "utf_8_sig")
+
+for name in f:
+    doc1 = DocxTemplate("test.docx")
+    context1 = {'Name': name.rstrip(), 'Olympiad': Olympiad, 'Date1': Date1, 'Date2': Date2}
+    doc1.render(context1)
+    doc1.save("{}_olympiad.docx".format(name.rstrip()))
+    convert("{}_olympiad.docx".format(name.rstrip()))
+    z.write("{}_olympiad.pdf".format(name.rstrip()))
+f.close()
+z.close()
